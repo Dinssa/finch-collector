3dfc227b8d7655d905033c8c0dc86f334d5fa6d9
dinssa <manny@dinssa.com>
2023-06-27 22:51:33 +0100

    Add create and edit finch ability

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Finch
 
@@ -15,3 +16,16 @@ def finch_index(request):
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finch/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finch/{finch.id}'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'species', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
