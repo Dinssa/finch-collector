6cf481a7003689040b2c7cbacfd8030239cebcf8
dinssa <manny@dinssa.com>
2023-06-27 21:11:43 +0100

    Add dummy finches data

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
-
+finches = [
+    {'name': 'Zebra Finch', 'species': 'Taeniopygia guttata', 'description': 'Small, gray and white finch with a distinctive red beak', 'age': 1},
+    {'name': 'Gouldian Finch', 'species': 'Erythrura gouldiae', 'description': 'Colorful finch with a purple chest, yellow belly, and green back', 'age': 2},
+    {'name': 'Society Finch', 'species': 'Lonchura domestica', 'description': 'Small, brown and white finch with a pleasant song', 'age': 3},
+    {'name': 'Java Sparrow', 'species': 'Lonchura oryzivora', 'description': 'Large, gray and white finch with a distinctive pink beak', 'age': 1},
+    {'name': 'Canary', 'species': 'Serinus canaria', 'description': 'Small, yellow finch with a beautiful song', 'age': 2},
+]
 
 def home(request):
     return render(request, 'home.html')
