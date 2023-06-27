b9d20c9d741c1f86776eca02944e58fc05418363
dinssa <manny@dinssa.com>
2023-06-27 22:15:37 +0100

    View finch details

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,7 @@ def about(request):
 def finch_index(request):
     finches = Finch.objects.all()
     return render(request, 'finch/index.html', {'finches': finches})
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finch/detail.html', {'finch': finch})
